0c7f1c0ee7982d32f5c87ee55ea1dfab9156a1bd
Alex Taffe <alex.taffe@gmail.com>
2018-11-14 15:42:37 -0500

    Refactor sign in button to be in the menu bar

--- BetteReddit/AppDelegate.m
+++ BetteReddit/AppDelegate.m
@@ -13,6 +13,7 @@
 @interface AppDelegate ()
 
 - (IBAction)saveAction:(id)sender;
+@property (strong, nonatomic) OIDRedirectHTTPHandler *redirectHTTPHandler;
 
 @end
 
@@ -44,6 +45,66 @@
     });
 
 
+}
+- (IBAction)newSignin:(id)sender {
+    static NSString *const kSuccessURLString = @"alextaffe.BetteReddit://oauth2redirect/example-provider";
+    NSURL *successURL = [NSURL URLWithString:kSuccessURLString];
+
+    NSURL *authorizationEndpoint = [NSURL URLWithString:@"https://www.reddit.com/api/v1/authorize"];
+    NSURL *tokenEndpoint = [NSURL URLWithString:@"https://www.reddit.com/api/v1/access_token"];
+
+    OIDServiceConfiguration *configuration =
+    [[OIDServiceConfiguration alloc]
+     initWithAuthorizationEndpoint:authorizationEndpoint
+     tokenEndpoint:tokenEndpoint];
+
+    // Starts a loopback HTTP redirect listener to receive the code.  This needs to be started first,
+    // as the exact redirect URI (including port) must be passed in the authorization request.
+    self.redirectHTTPHandler = [[OIDRedirectHTTPHandler alloc] initWithSuccessURL:successURL];
+    NSURL *redirectURI = [self.redirectHTTPHandler startHTTPListener:nil];
+    NSLog(@"%@", redirectURI);
+
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSURL *url = [bundle URLForResource:@"config" withExtension:@"plist"];
+    NSDictionary *userDict = [[NSDictionary alloc] initWithContentsOfURL:url];
+
+    // builds authentication request
+    OIDAuthorizationRequest *request =
+    [[OIDAuthorizationRequest alloc] initWithConfiguration:configuration
+                                                  clientId:userDict[@"client_id"]
+                                              clientSecret:@""
+                                                    scopes:@[ @"identity", @"edit", @"flair", @"history", @"modconfig", @"modflair", @"modlog", @"modposts", @"modwiki", @"mysubreddits", @"privatemessages", @"read", @"report", @"save", @"submit", @"subscribe", @"vote", @"wikiedit", @"wikiread" ]
+                                               redirectURL:redirectURI
+                                              responseType:OIDResponseTypeCode
+                                      additionalParameters:@{
+                                                             @"duration": @"permanent"
+                                                             }];
+    // performs authentication request
+    __weak __typeof(self) weakSelf = self;
+    self.redirectHTTPHandler.currentAuthorizationFlow =
+    [OIDAuthState authStateByPresentingAuthorizationRequest:request
+                                                   callback:^(OIDAuthState *_Nullable authState,
+                                                              NSError *_Nullable error) {
+                                                       // Brings this app to the foreground.
+                                                       [[NSRunningApplication currentApplication]
+                                                        activateWithOptions:(NSApplicationActivateAllWindows |
+                                                                             NSApplicationActivateIgnoringOtherApps)];
+
+                                                       // Processes the authorization response.
+                                                       if (authState) {
+                                                           BRUser *loggedInUser = [[BRUser alloc] initWithAccessToken:authState];
+                                                           [loggedInUser loadUserDetails:^{
+                                                               NSData *serialized = [NSKeyedArchiver archivedDataWithRootObject:authState];
+                                                               [SAMKeychain setPasswordData:serialized forService:@"reddit" account:loggedInUser.username];
+                                                               [weakSelf.loggedinUsers addObject:loggedInUser];
+                                                               [[NSNotificationCenter defaultCenter] postNotificationName:@"LoggedInUserRefresh" object:weakSelf];
+                                                           }];
+
+                                                       } else {
+                                                           NSLog(@"Authorization error: %@", error.localizedDescription);
+                                                       }
+                                                       //[weakSelf setAuthState:authState];
+                                                   }];
 }
 
 
--- BetteReddit/Controller/MainViewController.h
+++ BetteReddit/Controller/MainViewController.h
@@ -11,7 +11,6 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface MainViewController : NSViewController
-- (IBAction)loginAction:(id)sender;
 @end
 
 NS_ASSUME_NONNULL_END
