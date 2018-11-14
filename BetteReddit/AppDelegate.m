381b657334db509295bea361e3059370140ce0ac
Alex Taffe <alex.taffe@gmail.com>
2018-11-14 17:17:28 -0500

    Fix crash when no user saved to keychain

--- BetteReddit/AppDelegate.m
+++ BetteReddit/AppDelegate.m
@@ -24,26 +24,27 @@
 
     self.loggedinUsers = [[NSMutableArray alloc] init];
 
-    NSArray *test = [SAMKeychain accountsForService:@"reddit"];
-    __weak __typeof(self) weakSelf = self;
+    NSArray *savedAccounts = [SAMKeychain accountsForService:@"reddit"];
+        if(savedAccounts){
+        __weak __typeof(self) weakSelf = self;
 
-    dispatch_semaphore_t semaphore = dispatch_semaphore_create(test.count - 1);
-    for(NSDictionary *account in test){
-        NSData *authStateData = [SAMKeychain passwordDataForService:@"reddit" account:account[@"acct"]];
-        OIDAuthState *authState = (OIDAuthState *) [NSKeyedUnarchiver unarchiveObjectWithData:authStateData];
+        dispatch_semaphore_t semaphore = dispatch_semaphore_create(savedAccounts.count - 1);
+        for(NSDictionary *account in savedAccounts){
+            NSData *authStateData = [SAMKeychain passwordDataForService:@"reddit" account:account[@"acct"]];
+            OIDAuthState *authState = (OIDAuthState *) [NSKeyedUnarchiver unarchiveObjectWithData:authStateData];
 
-        BRUser *user = [[BRUser alloc] initWithAccessToken:authState];
-        [user loadUserDetails:^{
-            [weakSelf.loggedinUsers addObject:user];
-            dispatch_semaphore_signal(semaphore);
-        }];
+            BRUser *user = [[BRUser alloc] initWithAccessToken:authState];
+            [user loadUserDetails:^{
+                [weakSelf.loggedinUsers addObject:user];
+                dispatch_semaphore_signal(semaphore);
+            }];
+        }
+        dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^(void){
+            dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+            self.currentUser = self.loggedinUsers.firstObject;
+            [[NSNotificationCenter defaultCenter] postNotificationName:@"LoggedInUserRefresh" object:weakSelf];
+        });
     }
-    dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^(void){
-        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
-        self.currentUser = self.loggedinUsers.firstObject;
-        [[NSNotificationCenter defaultCenter] postNotificationName:@"LoggedInUserRefresh" object:weakSelf];
-    });
-
 
 }
 - (IBAction)newSignin:(id)sender {
