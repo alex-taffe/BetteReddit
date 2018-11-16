32f5c4707fd6a62a93f364a96b4c2703f0eed417
Alex Taffe <alex.taffe@gmail.com>
2018-11-16 11:27:02 -0500

    Update like button and likes color based on state

--- BetteReddit/View/PostTableViewCell.m
+++ BetteReddit/View/PostTableViewCell.m
@@ -39,6 +39,35 @@
 }
 
 -(void)updateColorScheme{
-    
+    NSColor *upColor;
+    NSColor *downColor;
+    NSColor *labelColor;
+
+    if(self.post.likes == 1){
+        upColor =  [NSColor colorNamed:@"UpvoteColor"];
+        downColor = NSColor.textColor;
+        labelColor = [NSColor colorNamed:@"UpvoteColor"];
+    } else if(self.post.likes == -1){
+        upColor =  NSColor.textColor;
+        downColor = [NSColor colorNamed:@"DownvoteColor"];
+        labelColor = [NSColor colorNamed:@"DownvoteColor"];
+    } else {
+        upColor = NSColor.textColor;
+        downColor = NSColor.textColor;
+        labelColor = NSColor.textColor;
+    }
+
+    NSMutableAttributedString *upTitle = [[NSMutableAttributedString alloc] initWithAttributedString:self.upArrow.attributedTitle];
+    NSMutableAttributedString *downTitle = [[NSMutableAttributedString alloc] initWithAttributedString:self.downButton.attributedTitle];
+    NSRange upRange = NSMakeRange(0, upTitle.length);
+    NSRange downRange = NSMakeRange(0, downTitle.length);
+
+    [upTitle addAttribute:NSForegroundColorAttributeName value:upColor range:upRange];
+    [downTitle addAttribute:NSForegroundColorAttributeName value:downColor range:downRange];
+
+    self.upArrow.attributedTitle = upTitle;
+    self.downButton.attributedTitle = downTitle;
+
+    self.postLikes.textColor = labelColor;
 }
 @end
